517f3779cdfe5132e52b55b7fe3c5174f061d58e
Charles GTE <charlesgte31@gmail.com>
2026-03-27 23:07:06 +0100

    feat: sending database ping result for each databases to portabase server

--- src/services/status.rs
+++ src/services/status.rs
@@ -8,6 +8,8 @@ use crate::settings::CONFIG;
 use reqwest::Client;
 use std::error::Error;
 use std::sync::Arc;
+use futures_util::future::try_join_all;
+use crate::domain::factory::DatabaseFactory;
 
 pub struct StatusService {
     ctx: Arc<Context>,
@@ -25,14 +27,20 @@ impl StatusService {
     pub async fn ping(&self, databases: &[DatabaseConfig]) -> Result<PingResult, Box<dyn Error>> {
         let edge_key = &self.ctx.edge_key;
 
-        let databases_payload: Vec<DatabasePayload> = databases
-            .iter()
-            .map(|db| DatabasePayload {
-                name: &db.name,
-                dbms: &db.db_type.as_str(),
-                generated_id: &db.generated_id,
+        let databases_payload: Vec<DatabasePayload> = try_join_all(
+            databases.into_iter().map(|db| async move {
+                let db_engine = DatabaseFactory::create_for_backup(db.clone()).await;
+
+                let reachable = db_engine.ping().await?;
+
+                Ok::<DatabasePayload, anyhow::Error>(DatabasePayload {
+                    name: &db.name,
+                    dbms: &db.db_type.as_str(),
+                    generated_id: &db.generated_id,
+                    ping_status: reachable,
+                })
             })
-            .collect();
+        ).await?;
 
         let version_str = CONFIG.app_version.as_str();
         let result = self
